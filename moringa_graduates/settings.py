c83a97f136ac24ca291fb60c738497f5978722c5
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-23 14:13:34 +0300

    feature: configure cloudinary for image storage

--- moringa_graduates/settings.py
+++ moringa_graduates/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'app.apps.AppConfig',
     'users.apps.UsersConfig'
 ]
