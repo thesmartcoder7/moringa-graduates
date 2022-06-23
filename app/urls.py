b751e476a923ef5db8963dac29c62b2ea1631b49
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-23 20:27:17 +0300

    style: structure and style the developers page

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.home, name='app-home')
+    path('', views.home, name='app-home'),
+    path('developers/', views.developers, name='app-developers')
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app/index.html')
+
+
+
+def developers(request):
+    return render(request, 'app/developers.html')
