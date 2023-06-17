b4497259ee4f7671472a472319cf10d644a9a055
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-06-17 23:41:06 +0300

    docs: document the views file

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,31 @@
+"""
+This file contains the Django form for the ProjectForm class.
+
+The ProjectForm is a ModelForm that is used to create or update Project instances.
+It specifies the fields and validation rules for creating or updating a Project object.
+
+Attributes:
+    Meta (class): The Meta inner class defines metadata for the form, such as the model it is associated with and the fields to include.
+
+Methods:
+    None
+
+"""
+
 from django import forms
 from app.models import *
 
 
 class ProjectForm(forms.ModelForm):
     class Meta:
+        """
+        Metadata for the ProjectForm class.
+
+        Attributes:
+            model (Model): The model class associated with the form (Project).
+            fields (list): The fields from the model that should be included in the form (title, image, description, link).
+
+        """
         model = Project
         fields = [
             'title',
--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
+"""
+This file contains Django views for handling different HTTP requests and rendering templates.
+
+Functions:
+    home(request): Renders the home page template.
+    developers(request): Renders the developers page template and displays a list of all developers.
+    dev_profile(request, username): Renders the developer profile page template for a specific user.
+    dashboard(request): Renders the dashboard page template for the authenticated user.
+    add_project(request): Renders the add project page template and handles the creation of a new project.
+    search(request): Handles the search functionality and renders the search page template.
+
+"""
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from app.models import Project
@@ -5,14 +18,29 @@ from django.contrib.auth.decorators import login_required
 from app.forms import *
 
 
-
-# Create your views here.
 def home(request):
+    """
+    Renders the home page template.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object containing the rendered home page template.
+    """
     return render(request, 'app/index.html')
 
 
-
 def developers(request):
+    """
+    Renders the developers page template and displays a list of all developers.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object containing the rendered developers page template.
+    """
     all_devs = User.objects.all()
     context = {
         'all_devs': all_devs
@@ -20,8 +48,17 @@ def developers(request):
     return render(request, 'app/developers.html', context)
 
 
-
 def dev_profile(request, username):
+    """
+    Renders the developer profile page template for a specific user.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        username (str): The username of the user whose profile is being accessed.
+
+    Returns:
+        HttpResponse: The HTTP response object containing the rendered developer profile page template.
+    """
     user = User.objects.get(username=username)
     user_skills = user.profile.skills.split()
     user_projects = Project.objects.filter(user=user)
@@ -33,9 +70,17 @@ def dev_profile(request, username):
     return render(request, 'app/dev_profile.html', context)
 
 
-
 @login_required
 def dashboard(request):
+    """
+    Renders the dashboard page template for the authenticated user.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object containing the rendered dashboard page template.
+    """
     user = User.objects.get(username=request.user.username)
     user_skills = user.profile.skills.split()
     user_projects = Project.objects.filter(user=user)
@@ -47,9 +92,17 @@ def dashboard(request):
     return render(request, 'app/dashboard.html', context)
 
 
-
 @login_required
 def add_project(request):
+    """
+    Renders the add project page template and handles the creation of a new project.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object containing the rendered add project page template.
+    """
     add_form = ProjectForm()
     context = { 
         'add_form': add_form,
@@ -74,8 +127,16 @@ def add_project(request):
     return render(request, 'app/add_project.html', context)
 
 
-
 def search(request):
+    """
+    Handles the search functionality and renders the search page template.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object containing the rendered search page template.
+    """
     if request.method == 'POST':
         all_users = User.objects.all()
         filtered_users = []
@@ -87,6 +148,5 @@ def search(request):
                     or term.lower() in user.last_name.lower() and user not in filtered_users and term != "" \
                     or term.lower() in user.profile.skills.lower() and user not in filtered_users and term != "":
                     filtered_users.append(user)
-
 
     return render(request, 'app/search.html', {'all_devs': filtered_users})
