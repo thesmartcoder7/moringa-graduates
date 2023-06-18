52c2926fa204c5ae113697e4b72c8c54d5918af4
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-06-18 23:50:19 +0300

    docs: document the signals

--- users/signals.py
+++ users/signals.py
@@ -4,14 +4,36 @@ from users.models import Profile
 from django.dispatch import receiver
 
 
-
 @receiver(post_save, sender=User)
 def create_profile(instance, created, **kwargs):
+    """
+    Signal receiver function to create a profile for a newly created user.
+
+    Args:
+        instance (User): The newly created User instance.
+        created (bool): Flag indicating if the User instance was created or updated.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+
+    """
     if created:
         Profile.objects.create(user=instance)
 
 
-
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
+    """
+    Signal receiver function to save the profile of a user.
+
+    Args:
+        sender (User): The User model.
+        instance (User): The User instance.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+
+    """
     instance.profile.save()
